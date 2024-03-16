1994101d2d62cfdf627543fb62749658ac5079bb
iliakovalenko <ilia.kovalenko@remme.io>
2024-03-16 18:03:17 +0200

    example service + youtube api added

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -1,7 +1,7 @@
 export const databaseConfig = {
-    host: 'youtube-data.cpkc6iime28n.ap-south-1.rds.amazonaws.com',
-    port: 3306,
-    username: 'admin',
-    password: 'Makkovaothers',
-    database: 'youtube',
+    "host": "***",
+    "port": 3306,
+    "username": "admin",
+    "password": "***",
+    "database": "",
 }
