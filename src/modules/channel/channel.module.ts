aa26c7967ffa42d9207761123b734fa4340a6e65
Illia Medvediev <bearillia@gmail.com>
2024-03-26 02:39:32 +0200

    Feat: refactoring

--- src/modules/channel/channel.module.ts
+++ src/modules/channel/channel.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import ChannelController from './channel.controller';
 import { ChannelService } from './channel.service';
+import { DatabaseModule } from '../../db/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [ChannelController],
   providers: [ChannelService],
 })
