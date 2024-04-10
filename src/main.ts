b5fe7561daa7e6250a03ac9b7701ba2967d0684c
iliakovalenko <ilia.kovalenko@remme.io>
2024-04-10 15:17:35 +0300

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { ValidationPipeOptions } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/exception-filters/http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ChannelRepository } from './db/repositories/channel.repository';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   console.log(process.env);
 
   const configService = app.get<ConfigService>(ConfigService);
@@ -19,7 +22,14 @@ async function bootstrap() {
 
   app.useGlobalPipes(validationPipeConfig);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.setGlobalPrefix('api');
 
+  const document = SwaggerModule.createDocument(app, new DocumentBuilder().build());
+  SwaggerModule.setup('api', app, document);
+
+  app.get(ChannelRepository).find().then(console.log)
+  
+  app.enableCors();
   await app.listen(port);
 
 }
--- src/modules/channel/dtos/pagination-params.dto.ts
+++ src/modules/channel/dtos/pagination-params.dto.ts
@@ -1,13 +1,16 @@
 import { IsNumber, Min, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class PaginationParams {
+  @ApiPropertyOptional()
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
   @Min(1)
   p?: number;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @Type(() => Number)
   @IsNumber()
